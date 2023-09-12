07324cd4f08a9683612d4766e9194f5d990435c5
murilo <61051754+muriloamancio00@users.noreply.github.com>
2023-09-12 17:50:01 -0300

    card component

--- app_locadora_carros/resources/js/components/Marcas.vue
+++ app_locadora_carros/resources/js/components/Marcas.vue
@@ -3,19 +3,13 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <!-- Inicio do card de Busca -->
-                <div class="card mb-3">
-                    <div class="card-header">Busca de Marcas</div>
-
-                    <div class="card-body">
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
                         <div class="form-row">
                             <div class="col mb-3">
 
-                                <input-container-component
-                                    titulo="ID"
-                                    id="InputId"
-                                    id-help="idHelp"
-                                    texto-ajuda="Opcional. Informe o ID da Marca">
-
+                                <input-container-component titulo="ID" id="InputId" id-help="idHelp"
+                                                           texto-ajuda="Opcional. Informe o ID da Marca">
                                     <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
                                            placeholder="ID">
                                 </input-container-component>
@@ -23,36 +17,29 @@
                             </div>
                             <div class="col mb-3">
 
-                                <input-container-component
-                                    titulo="Nome da Marca"
-                                    id="InputNome"
-                                    id-help="NomeHelp"
-                                    texto-ajuda="Opcional. Informe o Nome da Marca">
-
+                                <input-container-component titulo="Nome da Marca" id="InputNome" id-help="NomeHelp"
+                                                           texto-ajuda="Opcional. Informe o Nome da Marca">
                                     <input type="text" class="form-control" id="inputNome" aria-describedby="idNome"
                                            placeholder="Nome da marca">
                                 </input-container-component>
                             </div>
                         </div>
-                    </div>
-                    <div class="card-footer">
+                    </template>
+                    <template v-slot:rodape>
                         <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
-                    </div>
-                </div>
+                    </template>
+                </card-component>
                 <!-- Fim do card de Busca -->
 
                 <!-- Inicio do card de Listagem de Marcas -->
-
-                <div class="card">
-                    <div class="card-header">Relação das Marcas</div>
-
-                    <div class="card-body">
+                <card-component titulo="Relação das Marcas">
+                    <template v-slot:conteudo>
                         <table-component></table-component>
-                    </div>
-                    <div class="card-footer">
+                    </template>
+                    <template v-slot:rodape>
                         <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
-                    </div>
-                </div>
+                    </template>
+                </card-component>
 
                 <!-- Fim da Listagem de Marcas -->
             </div>
@@ -62,4 +49,10 @@
 
 <script>
 
+import {defineComponent} from "vue";
+import Card from "./Card.vue";
+
+export default defineComponent({
+    components: {Card}
+})
 </script>
